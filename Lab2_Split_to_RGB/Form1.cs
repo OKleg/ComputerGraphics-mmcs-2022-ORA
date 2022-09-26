d2ea4f43d0794118d773e5ed56f1ee48c66cf8c2
grchs <nedorubartem@gmail.com>
2022-09-27 00:30:22 +0300

    implemented Histograms

--- Lab2_Split_to_RGB/Form1.cs
+++ Lab2_Split_to_RGB/Form1.cs
@@ -8,6 +8,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Lab2_Split_to_RGB
 {
@@ -17,19 +18,18 @@ namespace Lab2_Split_to_RGB
         public Form1()
         {
             InitializeComponent();
-            //setting thing that picture transform to picturebox size
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
-            
+            //setting thing that whole picture will be in box
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             using (OpenFileDialog dlg = new OpenFileDialog())
             {
-                dlg.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+                dlg.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp"; //FileDialog filter for images
 
                 if (dlg.ShowDialog() == DialogResult.OK)
                 {
@@ -40,11 +40,19 @@ namespace Lab2_Split_to_RGB
             pictureBox1.Image = SelectedImage;
         }
 
+
         private void button2_Click(object sender, EventArgs e)
         {
             if (pictureBox1.Image == null) return; // doing nothing if image didnt selected
-            Bitmap bmp = new Bitmap(SelectedImage); //setting main bitmap 
+            Bitmap bmp = new Bitmap(SelectedImage); //setting original image bitmap 
             (int width, int height) = (bmp.Width, bmp.Height); //getting sizes of bitmap (jsut size of working area) 
+
+            int[] X_axis = new int[256]; // 0.255 X axis         
+
+            //Frequency of each 0..255 saturation 
+            int[] rc = new int[256];
+            int[] gc = new int[256];
+            int[] bc = new int[256];
 
             //creating array of bitmaps for R,G,B 
             Dictionary<Color, Bitmap> bmps = new Dictionary<Color, Bitmap>();
@@ -65,15 +73,27 @@ namespace Lab2_Split_to_RGB
                     bmps[Color.Green].SetPixel(x, y, Color.FromArgb(rgb.A, 0, rgb.G, 0));
                     bmps[Color.Blue].SetPixel(x, y, Color.FromArgb(rgb.A, 0, 0, rgb.B));
 
+                    //counting counts of each 0.255 part of color
+                    rc[rgb.R]++;
+                    gc[rgb.G]++;
+                    bc[rgb.B]++;
                 }
-            }
+            }        
 
-            //set images into imageboxes
+            //setting images to imageboxes
             pictureBox2.Image = bmps[Color.Red];
             pictureBox3.Image = bmps[Color.Green];
             pictureBox4.Image = bmps[Color.Blue];
 
+            //setting colors for histograms 
+            chart1.Series["Series1"].Color = Color.Red;
+            chart2.Series["Series1"].Color = Color.Green;
+            chart3.Series["Series1"].Color = Color.Blue;
 
+            //Draw diagrams by 2 collections, color saturation and count of pixels
+            chart1.Series["Series1"].Points.DataBindXY(X_axis, rc);   
+            chart2.Series["Series1"].Points.DataBindXY(X_axis, gc);
+            chart3.Series["Series1"].Points.DataBindXY(X_axis, bc);
         }
     }
 }
