a1485c2218cdc3a91749e9fe947ee4cbbd99b3dd
grchs <nedorubartem@gmail.com>
2022-09-26 20:54:34 +0300

    Added ability to browse image, added "Browse image" button and 4 pictureBoxes, implemented setting the selected photo into PictureBox

--- Lab2_Split_to_RGB/Form1.cs
+++ Lab2_Split_to_RGB/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,25 @@ namespace Lab2_Split_to_RGB
 {
     public partial class Form1 : Form
     {
+        Image SelectedImage;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    SelectedImage = Image.FromFile(dlg.FileName);
+                }
+
+            }
+            pictureBox1.Image = SelectedImage;
         }
     }
 }
