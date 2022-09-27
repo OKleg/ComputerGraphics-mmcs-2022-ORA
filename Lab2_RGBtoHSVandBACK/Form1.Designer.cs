163e17b6e4f133839925f03b4667468cdd48c3c7
Geluos <55185142+Geluos@users.noreply.github.com>
2022-09-28 02:40:13 +0300

    Add: RGBtoHSV Module

---
+++ Lab2_RGBtoHSVandBACK/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace Lab2_RGBtoHSVandBACK
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+			this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+			this.hScrollBar3 = new System.Windows.Forms.HScrollBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(380, 423);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 442);
+			this.button1.Margin = new System.Windows.Forms.Padding(4);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(379, 62);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "Browse image";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// hScrollBar1
+			// 
+			this.hScrollBar1.Location = new System.Drawing.Point(471, 39);
+			this.hScrollBar1.Maximum = 200;
+			this.hScrollBar1.Name = "hScrollBar1";
+			this.hScrollBar1.Size = new System.Drawing.Size(403, 29);
+			this.hScrollBar1.TabIndex = 6;
+			this.hScrollBar1.Value = 100;
+			this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+			// 
+			// hScrollBar2
+			// 
+			this.hScrollBar2.Location = new System.Drawing.Point(471, 98);
+			this.hScrollBar2.Maximum = 200;
+			this.hScrollBar2.Name = "hScrollBar2";
+			this.hScrollBar2.Size = new System.Drawing.Size(403, 29);
+			this.hScrollBar2.TabIndex = 7;
+			this.hScrollBar2.Value = 100;
+			this.hScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar2_Scroll);
+			// 
+			// hScrollBar3
+			// 
+			this.hScrollBar3.Location = new System.Drawing.Point(471, 160);
+			this.hScrollBar3.Maximum = 200;
+			this.hScrollBar3.Name = "hScrollBar3";
+			this.hScrollBar3.Size = new System.Drawing.Size(403, 29);
+			this.hScrollBar3.TabIndex = 8;
+			this.hScrollBar3.Value = 100;
+			this.hScrollBar3.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar3_Scroll);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(422, 39);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(18, 17);
+			this.label1.TabIndex = 9;
+			this.label1.Text = "H";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(422, 98);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(17, 17);
+			this.label2.TabIndex = 10;
+			this.label2.Text = "S";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(422, 160);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(17, 17);
+			this.label3.TabIndex = 11;
+			this.label3.Text = "V";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(495, 442);
+			this.button2.Margin = new System.Windows.Forms.Padding(4);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(379, 62);
+			this.button2.TabIndex = 12;
+			this.button2.Text = "CreateNew";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(936, 594);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.hScrollBar3);
+			this.Controls.Add(this.hScrollBar2);
+			this.Controls.Add(this.hScrollBar1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "HSV (made by Geluos)";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.HScrollBar hScrollBar1;
+		private System.Windows.Forms.HScrollBar hScrollBar2;
+		private System.Windows.Forms.HScrollBar hScrollBar3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button2;
+	}
+}
---
+++ Lab2_RGBtoHSVandBACK/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab2_RGBtoHSVandBACK
+{
+	public partial class Form1 : Form
+	{
+		Image SelectedImage;
+
+		public static Color ColorFromHSV(double hue, double saturation, double value)
+		{
+			int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+			double f = hue / 60 - Math.Floor(hue / 60);
+
+			value = value * 255;
+			int v = Convert.ToInt32(value);
+			int p = Convert.ToInt32(value * (1 - saturation));
+			int q = Convert.ToInt32(value * (1 - f * saturation));
+			int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+			if (hi == 0)
+				return Color.FromArgb(255, v, t, p);
+			else if (hi == 1)
+				return Color.FromArgb(255, q, v, p);
+			else if (hi == 2)
+				return Color.FromArgb(255, p, v, t);
+			else if (hi == 3)
+				return Color.FromArgb(255, p, q, v);
+			else if (hi == 4)
+				return Color.FromArgb(255, t, p, v);
+			else
+				return Color.FromArgb(255, v, p, q);
+		}
+
+		private double GetSaturation(double min, double max)
+		{
+			if (max == 0)
+			{
+				return 0;
+			}
+			else
+			{
+				return 1 - min / max;
+			}
+		}
+
+		private double GetHue(double R, double G, double B, double min, double max)
+		{
+			if (max == min)
+				return 0;
+
+			if (max == R && G>=B)
+			{
+				return 60 * (G - B) / (max - min);
+			}
+
+			if (max == R && G < B)
+			{
+				return 60 * (G - B) / (max - min) + 360;
+			}
+
+			if (max == G)
+			{
+				return 60 * (B - R) / (max - min) + 120;
+			}
+
+			if (max == B)
+			{
+				return 60 * (R - G) / (max - min) + 240;
+			}
+
+			return 0;
+		}
+
+		private double GetValue(double min, double max)
+		{
+			return max;
+		}
+
+		public Color TransformHSV(Color color, double h, double s, double v)
+		{
+			double R = (double)color.R / 255;
+			double G = (double)color.G / 255;
+			double B = (double)color.B / 255;
+
+			double max = Math.Max(R, Math.Max(G, B));
+			double min = Math.Min(R, Math.Min(G, B));
+
+			double H = (GetHue(R, G, B, min, max) + (h - 1)/2*360) % 360d;
+			double S = Math.Min(1, GetSaturation(min, max)*s);
+			double V = Math.Min(1, GetValue(min, max) * v);
+
+			return ColorFromHSV(H, S, V);
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+			pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		public void TranformImage(double h, double s, double v)
+		{
+			if (SelectedImage == null)
+				return;
+			Bitmap bitmap_hsv = (Bitmap)(SelectedImage.Clone());
+
+			(int width, int height) = (bitmap_hsv.Width, bitmap_hsv.Height);
+			Bitmap newbmp = new Bitmap(width, height);
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					Color clr = bitmap_hsv.GetPixel(x, y);
+					newbmp.SetPixel(x, y, TransformHSV(clr, h, s, v));
+				}
+			}
+
+			pictureBox1.Image = newbmp;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			using (OpenFileDialog dlg = new OpenFileDialog())
+			{
+				dlg.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp"; //FileDialog filter for images
+
+				if (dlg.ShowDialog() == DialogResult.OK)
+				{
+					SelectedImage = Image.FromFile(dlg.FileName);
+				}
+
+			}
+			pictureBox1.Image = SelectedImage;
+			//Color clr = ColorFromHSV(235, 0.55, 0.87);
+			//Color clr2 = TransformHSV(Color.FromArgb(255, 135, 12, 55), 1, 1, 1);
+			TranformImage((double)hScrollBar1.Value / 100, (double)hScrollBar2.Value / 100, (double)hScrollBar3.Value / 100);
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+		{
+
+		}
+
+		private void hScrollBar3_Scroll(object sender, ScrollEventArgs e)
+		{
+		}
+
+		private void hScrollBar2_Scroll(object sender, ScrollEventArgs e)
+		{
+			
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			TranformImage((double)hScrollBar1.Value / 100, (double)hScrollBar2.Value / 100, (double)hScrollBar3.Value / 100);
+			pictureBox1.Image.Save("SavedImage.png");
+			//pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+		}
+	}
+}
