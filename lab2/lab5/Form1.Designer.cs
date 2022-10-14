de74cd3ee1fea6314145827551c99c23b56297a6
OKleg <ol.krol@yandex.ru>
2022-10-14 23:40:14 +0300

    lab5

---
+++ lab2/lab5/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonL = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonTree = new System.Windows.Forms.Button();
+            this.buttonMidpoint = new System.Windows.Forms.Button();
+            this.buttonBezier = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonL
+            // 
+            this.buttonL.Location = new System.Drawing.Point(2, 175);
+            this.buttonL.Name = "buttonL";
+            this.buttonL.Size = new System.Drawing.Size(107, 23);
+            this.buttonL.TabIndex = 1;
+            this.buttonL.Text = "L-system";
+            this.buttonL.UseVisualStyleBackColor = true;
+            this.buttonL.Click += new System.EventHandler(this.buttonL_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(115, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(842, 511);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(2, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(107, 157);
+            this.textBox1.TabIndex = 3;
+            // 
+            // buttonTree
+            // 
+            this.buttonTree.Location = new System.Drawing.Point(2, 204);
+            this.buttonTree.Name = "buttonTree";
+            this.buttonTree.Size = new System.Drawing.Size(107, 23);
+            this.buttonTree.TabIndex = 4;
+            this.buttonTree.Text = "Tree";
+            this.buttonTree.UseVisualStyleBackColor = true;
+            // 
+            // buttonMidpoint
+            // 
+            this.buttonMidpoint.Location = new System.Drawing.Point(2, 233);
+            this.buttonMidpoint.Name = "buttonMidpoint";
+            this.buttonMidpoint.Size = new System.Drawing.Size(107, 23);
+            this.buttonMidpoint.TabIndex = 5;
+            this.buttonMidpoint.Text = "Midpoint";
+            this.buttonMidpoint.UseVisualStyleBackColor = true;
+            // 
+            // buttonBezier
+            // 
+            this.buttonBezier.Location = new System.Drawing.Point(2, 262);
+            this.buttonBezier.Name = "buttonBezier";
+            this.buttonBezier.Size = new System.Drawing.Size(107, 23);
+            this.buttonBezier.TabIndex = 6;
+            this.buttonBezier.Text = "Безье";
+            this.buttonBezier.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 528);
+            this.Controls.Add(this.buttonBezier);
+            this.Controls.Add(this.buttonMidpoint);
+            this.Controls.Add(this.buttonTree);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonL;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonTree;
+        private System.Windows.Forms.Button buttonMidpoint;
+        private System.Windows.Forms.Button buttonBezier;
+    }
+}
+
---
+++ lab2/lab5/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Color color;
+        Pen pen;
+        PaintEventArgs ev;
+        Point p;
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            color = Color.Black;
+            pen = new Pen(color);
+            PaintEventArgs ev = new PaintEventArgs(g, pictureBox1.ClientRectangle);
+            Point p = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+
+           
+        }
+        
+         private void F()
+        {
+
+        }
+        private void buttonL_Click(object sender, EventArgs e)
+        {
+         using ( FileStream fs = File.OpenRead("../../L.txt")){
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    textBox1.Text += temp.GetString(b)+"\n";
+
+                }
+            };
+            g.DrawLine(pen, p.X, p.Y, (int)((p.X)+10*Math.Tan(45)),  (int)((p.Y) + 10*Math.Tan(45)) );
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            p = e.Location;
+            g.DrawEllipse(pen, e.X - 1, e.Y - 1, 3, 3); 
+            pictureBox1.Invalidate();
+        }
+    }
+}
